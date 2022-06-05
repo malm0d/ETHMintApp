c0d88df9168fb1dfe66d73063b7bb68d5b1fbdea
Zhe Rui Tan <32306977+malcolmtzr@users.noreply.github.com>
2022-06-05 20:16:28 +0800

    Updates

---
+++ precache-manifest.9e4bb178942e53706809e2269db39dfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9635528d4972058ddc4d865a990e6920",
+    "url": "/ETHMintApp/index.html"
+  },
+  {
+    "revision": "655a0e43cdeb03a052d2",
+    "url": "/ETHMintApp/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "6ea7d265ad327b28e393",
+    "url": "/ETHMintApp/static/js/2.acb5779b.chunk.js"
+  },
+  {
+    "revision": "655a0e43cdeb03a052d2",
+    "url": "/ETHMintApp/static/js/main.79985787.chunk.js"
+  },
+  {
+    "revision": "6499dee1c87a1a352642",
+    "url": "/ETHMintApp/static/js/runtime-main.8efeb94e.js"
+  }
+]);
